0463130765709d3a359b6e1d3d48446f6ba46d68
jjuanramos <jjuanramos91@gmail.com>
2022-02-28 19:53:07 +0100

    finished 1.3

--- token/token.go
+++ token/token.go
@@ -27,7 +27,19 @@ const (
 	LET      = "LET"
 )
 
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
